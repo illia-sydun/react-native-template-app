c0c2ec73709fe918c8f2bd454cfc808bd98f46b4
Illia Sydun <illia.sydun@gmail.com>
2024-06-01 14:28:27 -0400

    setup: Configure ESLint, Prettier and TypeScript check on commit with Husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,34 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-    extends: ['expo', 'prettier'],
-    plugins: ['prettier'],
+    env: {
+        es6: true,
+    },
+    root: true,
+    plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    extends: [
+        'expo',
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended',
+        'airbnb',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:import/typescript',
+        'prettier',
+    ],
     rules: {
         'prettier/prettier': 'error',
+        'no-case-declarations': 'off',
+        'no-unused-expressions': 'off',
     },
 };
